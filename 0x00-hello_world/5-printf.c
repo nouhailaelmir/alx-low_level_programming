052474717c9cbcf28163a5ac48b5d97acf028c37
nouhailaelmir <nouhailaelmir4@gmail.com>
2023-02-16 09:07:34 -0800

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
