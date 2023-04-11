cfe94a2fec188ff4a9c37ebc44f7ab4c7a519070
nouhailaelmir <nouhailaelmir4@gmail.com>
2023-04-10 18:04:07 -0700

    commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+* create_file - creates a file
+*
+* with 600 perms (do not change if it exists)
+*
+* @filename: A pointer to the name of the file to create.
+* @text_content: A pointer to a string to write to the file.
+*
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int length = 0, inlen = 0;
+	char *ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
+	}
+
+	if (close(file) == -1 || inlen != length)
+		return (-1);
+	return (1);
+}
